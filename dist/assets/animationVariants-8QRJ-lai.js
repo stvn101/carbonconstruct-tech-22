b3876e03462243e710c885d2a5c56d3722d0d20e
Steven Jenkins <stvnjnkns101@gmail.com>
2025-08-20 01:28:55 +1000

    chore: ignore build artifacts and bun.lockb

--- dist/assets/animationVariants-8QRJ-lai.js
+++
@@ -1,2 +0,0 @@
-const a={initial:{opacity:0,y:20},animate:{opacity:1,y:0,transition:{duration:.5}}},t={initial:{opacity:0},animate:{opacity:1,transition:{staggerChildren:.15}}},i={initial:{opacity:0,scale:.9,rotate:-1},animate:{opacity:1,scale:1,rotate:-1,transition:{duration:.7,ease:[.4,0,.2,1]}}};export{i as d,a as f,t as s};
-//# sourceMappingURL=animationVariants-8QRJ-lai.js.map
